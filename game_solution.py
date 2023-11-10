227297685343c9dfa7b0dd88cbc7d75d1d9a63e1
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2023-11-10 13:00:17 +0000

    map generates now based on user click saved to coords.txt for later use

--- game_solution.py
+++ game_solution.py
@@ -4,10 +4,7 @@
 # tested on python 3.8.10
 # initial commit: 09-11-2023
 from tkinter import Tk
-from tkinter import Button as TkButton, Label as TkLabel, Menu as TkMenu, Frame as TkFrame, Canvas as TkCanvas
-import random
-
-from click import command
+from tkinter import Button as TkButton, Label as TkLabel, Menu as TkMenu, Frame as TkFrame, Canvas as TkCanvas, messagebox as messagebox
 
 # ---All functions go here---
 
@@ -18,21 +15,36 @@ class MapGenerator:
         self.width = width
         self.height = height
         self.cell_size = cell_size
-        self.canvas = TkCanvas(master, width=width * cell_size, height=height * cell_size)
+        self.canvas = TkCanvas(master, width=width * cell_size, height=height * cell_size, bg="white")
         self.canvas.pack()
 
         self.map = [[0 for _ in range(width)] for _ in range(height)]  # Initialize the map
+        self.path_coordinates = []  # To store the coordinates of the selected path
 
-    def generate_route(self):
-        current_position = (0, random.randint(0, self.height - 1))
-        self.map[current_position[1]][current_position[0]] = 1  # Mark the starting point
+        # Binding the left mouse click event to the canvas
+        self.canvas.bind("<Button-1>", self.on_canvas_click)
 
-        while current_position[0] < self.width - 1:
-            movement = random.choice([-1, 0, 1])
-            new_y = current_position[1] + movement
-            new_y = max(0, min(new_y, self.height - 1))
-            current_position = (current_position[0] + 1, new_y)
-            self.map[current_position[1]][current_position[0]] = 1
+    def on_canvas_click(self, event):
+        # Calculate the grid coordinates based on the mouse click position
+        x = event.x // self.cell_size
+        y = event.y // self.cell_size
+
+        # Toggle the path status of the clicked cell
+        if self.map[y][x] == 0:
+            self.map[y][x] = 1
+            self.path_coordinates.append((x, y))
+            self.canvas.create_rectangle(x * self.cell_size, y * self.cell_size,
+                                         (x + 1) * self.cell_size, (y + 1) * self.cell_size,
+                                         fill="black")
+        else:
+            self.map[y][x] = 0
+            self.path_coordinates.remove((x, y))
+            self.canvas.create_rectangle(x * self.cell_size, y * self.cell_size,
+                                         (x + 1) * self.cell_size, (y + 1) * self.cell_size,
+                                         fill="white")
+
+    def get_path_coordinates(self):
+        return self.path_coordinates
 
     def draw_map(self):
         for y, row in enumerate(self.map):
@@ -75,25 +87,29 @@ class Game:
         
         self.menu.add_command(label="Exit", command=self.root.quit) # exit button
         
-        #generate map
+        # Create and display the map
         map_generator = MapGenerator(self.canvas, 32, 24, cell_size=25)
-        map_generator.generate_route()
-        map_generator.draw_map()
+        self.map_generator = map_generator
 
 
         self.root.mainloop()
 
     def new_game(self):
-        print("New Game")
+        # Print the path coordinates to the console
+        path_coordinates = self.map_generator.get_path_coordinates()
+        print("Predefined Path Coordinates:")
+        for coordinate in path_coordinates:
+            print(coordinate)
+        messagebox.showinfo("New Game", "Starting a new game!")
 
     def save_game(self):
-        print("Save Game")
+        messagebox.showinfo("Save Game", "Game saved!")
 
     def load_game(self):
-        print("Load Game")
+        messagebox.showinfo("Load Game", "Game loaded!")
 
     def about(self):
-        print("About")
+        messagebox.showinfo("About", "Tower Defense Game by Your Name")
 
 
 # ---RUN PROGRAM--- #
